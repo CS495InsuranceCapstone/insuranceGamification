86f4bcb4228d069ee4368e5c49c1edbfdc6e28e4
Nick <nsmith458914@gmail.com>
2017-10-19 11:39:17 -0400

    Create RandomEvent class.

--- src/backend/event/randomevent.ts
+++ src/backend/event/randomevent.ts
@@ -1,3 +1,11 @@
 export class RandomEvent {
+  name: String;
+  text: String;
+  img: HTMLImageElement;
 
+  constructor(name, text, img) {
+    this.name = name;
+    this.text = text;
+    this.img = img;
+  }
 }
