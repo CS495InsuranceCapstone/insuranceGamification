a3b6f29844abe7db5eb4186aea682d4d0e221c83
Nick <nsmith458914@gmail.com>
2017-10-22 14:20:39 -0400

    Create EventQueue in randomevent.ts

--- src/backend/event/randomevent.ts
+++ src/backend/event/randomevent.ts
@@ -1,11 +1,46 @@
-export class RandomEvent {
+export class Event {}
+
+export class RandomEvent extends Event {
   name: String;
   text: String;
   img: HTMLImageElement;
 
   constructor(name, text, img) {
+    super()
     this.name = name;
     this.text = text;
     this.img = img;
+  }
+}
+
+export class EventQueue {
+  event: Event;
+  next: EventQueue;
+
+  constructor(event) {
+    this.event = event;
+    this.next = null;
+  }
+
+  add(nextEvent) {
+    if (this.next != null) {
+      this.next = new EventQueue(nextEvent);
+    } else {
+      this.next.add(nextEvent);
+    }
+  }
+
+  getNextEvent() {
+    var retEvent = this.event;
+    this.moveUp();
+    return retEvent;
+  }
+
+  moveUp() {
+    if (this.next != null) {
+      this.event = this.next.event;
+      this.next = this.next.next;
+      this.next.moveUp();
+    }
   }
 }
