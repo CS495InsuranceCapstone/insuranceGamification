33518b7f89ac907b8af722f7fef95782332d44c8
Nick <nsmith458914@gmail.com>
2017-10-22 14:21:23 -0400

    Move eventqueue to its own file.

--- src/backend/event/randomevent.ts
+++ src/backend/event/randomevent.ts
@@ -12,35 +12,3 @@ export class RandomEvent extends Event {
     this.img = img;
   }
 }
-
-export class EventQueue {
-  event: Event;
-  next: EventQueue;
-
-  constructor(event) {
-    this.event = event;
-    this.next = null;
-  }
-
-  add(nextEvent) {
-    if (this.next != null) {
-      this.next = new EventQueue(nextEvent);
-    } else {
-      this.next.add(nextEvent);
-    }
-  }
-
-  getNextEvent() {
-    var retEvent = this.event;
-    this.moveUp();
-    return retEvent;
-  }
-
-  moveUp() {
-    if (this.next != null) {
-      this.event = this.next.event;
-      this.next = this.next.next;
-      this.next.moveUp();
-    }
-  }
-}
