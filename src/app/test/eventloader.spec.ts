154cf309027d1e23e025d582cb51a49d03d584c6
Nick <nsmith458914@gmail.com>
2017-10-24 14:33:45 -0400

    Make tests for getting events from lists.

--- src/app/test/eventloader.spec.ts
+++ src/app/test/eventloader.spec.ts
@@ -1,4 +1,5 @@
 import { RandomEventLoader } from '../backend/event/eventloader'
+import { RandomEvent } from '../backend/event/event'
 
 describe('RandomEventLoader', () => {
 
@@ -8,8 +9,20 @@ describe('RandomEventLoader', () => {
     eventLoader = new RandomEventLoader();
   });
 
-  it('should be good', () => {
-    expect(true).toBe(true);
+  it('should get positive event of proper index', () => {
+    expect(JSON.stringify(eventLoader.getPositiveEvent(0))).toBe(JSON.stringify(new RandomEvent(
+      "{Billy Johnson}",
+      "{You have just gotten a 10% bonus from work. How will you handle the extra money you have just recieved?}",
+      "{https://www.wikihow.com/images/c/c7/Raise-Money-Step-30.jpg}"
+    )));
+  });
+
+  it('should get negative event of proper index', () => {
+    expect(JSON.stringify(eventLoader.getNegativeEvent(0))).toBe(JSON.stringify(new RandomEvent(
+      "{Billy Johnson}",
+      "{You have gotten into a car accident, that has left your car completely damaged! How will you handle this terrible situation?}",
+      "{https://familydoctor.org/wp-content/uploads/2000/09/24611539_l-705x470.jpg}"
+    )));
   });
 
 });
