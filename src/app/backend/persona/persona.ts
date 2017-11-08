19f8c469fcb75d39f7a1e267073b8233f530bb7c
Unknown <brittanistokes@ymail.com>
2017-11-07 23:48:20 -0500

    Create persona class.

---
+++ src/app/backend/persona/persona.ts
@@ -0,0 +1,22 @@
+export class Persona {
+    
+      name: string;
+      sex: string;
+      age: string;
+      maritalStatus: string;
+      dependents: string;
+      profession: string;
+      salary: string;
+      
+    
+      constructor(name: string,sex: string,age: string,maritalStatus: string,dependents: string,profession: string,salary: string) {
+        this.name = name;
+        this.sex = sex;
+        this.age = age;
+        this.maritalStatus = maritalStatus;
+        this.dependents = dependents;
+        this.profession = profession;
+        this.salary = salary;
+      }
+    
+    }
