030a84c653194ca9be8b11cf2fb75bee56d52db1
Nick <nsmith458914@gmail.com>
2017-11-15 09:34:40 -0500

    Add flags to events.

--- src/app/test/eventloader.spec.ts
+++ src/app/test/eventloader.spec.ts
@@ -10,19 +10,23 @@ describe('RandomEventLoader', () => {
   });
 
   it('should get positive event of proper index', () => {
-    expect(JSON.stringify(eventLoader.getPositiveEvent(0))).toBe(JSON.stringify(new RandomEvent(
+    let event = new RandomEvent(
       "Your Bonus Check!",
       "You have just gotten a 10% bonus from work. How will you handle the extra money you have just recieved?",
       "https://www.wikihow.com/images/c/c7/Raise-Money-Step-30.jpg"
-    )));
+    );
+    event.flags = {"age":"> 25","salary":"> 40000"};
+    expect(JSON.stringify(eventLoader.getPositiveEvent(0))).toBe(JSON.stringify(event));
   });
 
   it('should get negative event of proper index', () => {
-    expect(JSON.stringify(eventLoader.getNegativeEvent(0))).toBe(JSON.stringify(new RandomEvent(
+    let event = new RandomEvent(
       "Car Accident",
       "You have gotten into a car accident, that has left your car completely damaged! How will you handle this terrible situation?",
       "https://familydoctor.org/wp-content/uploads/2000/09/24611539_l-705x470.jpg"
-    )));
+    );
+    event.flags = {"age":"<  25","salary":"<  60000"};
+    expect(JSON.stringify(eventLoader.getNegativeEvent(0))).toBe(JSON.stringify(event));
   });
 
   it('should no longer hold random event after it\'s been retrieved', () => {
@@ -43,11 +47,13 @@ describe('PredefinedEventLoader', () => {
   });
 
   it('should give a event when getNextEvent() is called', () => {
-    expect(JSON.stringify(eventLoader.getNextEvent())).toBe(JSON.stringify(new PredefinedEvent(
+    let event = new PredefinedEvent(
       "Sign the Deed",
       "You just made the biggest purchase of your life!",
       "null"
-    )));
+    );
+    event.flags = {};
+    expect(JSON.stringify(eventLoader.getNextEvent())).toBe(JSON.stringify(event));
   });
 
   it('should remove an event from the loader after it\'s been returned', () => {
