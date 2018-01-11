06c2145d4ffcb92ad2451bfd5f96e4476e221356
Nick <nsmith458914@gmail.com>
2018-01-11 13:39:05 -0500

    Add BankAccount class.

--- karma.conf.js
+++ karma.conf.js
@@ -8,8 +8,10 @@ module.exports = function (config) {
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
+      require('karma-firefox-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
+      require('karma-webpack'),
       require('@angular/cli/plugins/karma')
     ],
     client:{
---
+++ src/app/backend/bank/bank_accounts.ts
@@ -0,0 +1,18 @@
+class BankAccount {
+
+  private balance: number = 0;
+
+  withdraw(amount: number): number {
+    if (amount > this.balance) {
+      throw new RangeError();
+    } else {
+      this.balance -= amount;
+      return amount;
+    }
+  }
+
+  deposit(amount: number): void {
+    this.balance += amount;
+  }
+
+}
