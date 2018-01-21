b0c20210b5fc35c6df457ad16226b1c0589ba184
Unknown <brittanistokes@ymail.com>
2018-01-21 18:44:18 -0500

    Add game class

--- src/app/backend/game/game.ts
+++ src/app/backend/game/game.ts
@@ -7,26 +7,42 @@ export class Game{
     private queue: EventQueue;
     persona: Persona;
     event:Event;
+    counteract: Game;
     
 
+    
     constructor(event:Event,persona: Persona){
         this.persona = persona;
         this.event = event;
         this.queue = new EventQueue(event);
+        this.start();
 
+    }
+
+    start(){
+        
     }
 
     private presentEvent(event: Event): void{
         if(this.queue == null){
-            this.queue = new EventQueue(event);
+            this.end();            
         }else{
             this.queue.getNextEvent(event);
         }
     }
 
+    private end(): void{
+        if(this.queue.next.getNextEvent(event) == null){
+            alert("There are no more events to encounter. You have won the game!");
+        }else if(this.counteract.counteractEvent == null){
+            alert("You were unabale to counteract the event. Game is over!");
+        }  
+    }
+
     private counteractEvent(option:() => void): void{
 
     }
+
     
 
 
