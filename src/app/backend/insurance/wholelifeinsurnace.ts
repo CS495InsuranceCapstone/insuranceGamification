b5d8fa2ec8254800e09507ed3aaa7db54a90de60
Nick <nsmith458914@gmail.com>
2018-01-21 16:25:44 -0500

    Add agePremiumPaidTo to WholeLifeInsurance.

--- src/app/backend/insurance/wholelifeinsurnace.ts
+++ src/app/backend/insurance/wholelifeinsurnace.ts
@@ -1,9 +1,25 @@
 import { LifeInsurancePolicy } from './lifeinsurance'
+import { Persona } from '../persona/persona'
 
 class TermLifeInsurancePolicy extends LifeInsurancePolicy {
+
+  private agePremiumPaidTo: number;
+  private prewriteClass: PrewriteClass;
+
+  constructor(persona: Persona, desiredDeathPayOut: number, agePremiumPaidTo: number) {
+    super(persona, desiredDeathPayOut);
+    this.agePremiumPaidTo = agePremiumPaidTo;
+  }
 
   protected definePolicy(): void {
     // TODO: Use passed persona to calculate policy pieces
   }
 
+}
+
+enum PrewriteClass {
+  Preferred,
+  NotSoPreferred,
+  Okay,
+  Bad
 }
