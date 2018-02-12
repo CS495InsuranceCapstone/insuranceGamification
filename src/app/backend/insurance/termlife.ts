23b39ce27d1c5e2825c36528c3ca30d3ec9958d9
Unknown <brittanistokes@ymail.com>
2018-02-11 22:44:55 -0500

    Update Term Life.

--- src/app/backend/insurance/termlife.ts
+++ src/app/backend/insurance/termlife.ts
@@ -24,13 +24,13 @@ class TermLifeInsurancePolicy extends LifeInsurancePolicy {
   private update(choice: number): void{
     if(choice == 20){
       if(this.persona.age >=20 && this.persona.age <= 60){
-        this.premium++;
+        this.setPremium();
       }else{
         this.premium = 0;
       }
     }else if(choice ==30){
       if(this.persona.age >=20 && this.persona.age <= 55){
-        this.premium++;
+        this.setPremium();
       }else{
         this.premium = 0;      
       }
@@ -39,15 +39,16 @@ class TermLifeInsurancePolicy extends LifeInsurancePolicy {
 
   setPremium(){
     //No clear calculations on term life insurance.
-    let gender = this.persona.sex;
+    let gender = this.persona.sex == 'M' ? 1.5 : 1;
     let age = this.persona.age;
     let health = this.persona.health;
   }
 
   endPremium(){
     let end = this.term;
-    for(let i = 0; i < end; i++){
-      //should loop through the years and end when the term is up.
+    let count = 0;
+    if(count < end){
+      count++;
     }
   }
 
